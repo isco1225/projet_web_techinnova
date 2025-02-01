e052f09fca76f1a64249d818efaaa87640f73518
BGED <166635010+enockblitto@users.noreply.github.com>
2025-02-01 11:52:05 +0000

    Add files via upload

---
+++ Techinnova/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1000,  /* Durée de l'animation */
+        once: true       /* Lancer l'animation une seule fois */
+    });
+});
